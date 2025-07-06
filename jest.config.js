c492058b2b52fd8c63b45ede38521bf61212be02
Samuel Holley <samuelholleyai@gmail.com>
2025-07-05 21:23:08 -0700

    test: remove legacy react-native Jest preset  types: add CreateTaskInput export to unblock build

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,11 @@
 module.exports = {
-  preset: 'react-native',
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  transformIgnorePatterns: [],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  roots: ['<rootDir>/src'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 };
---
+++ src/types/tasks.ts
@@ -0,0 +1,7 @@
+export type CreateTaskInput = {
+  // TODO: Add actual fields as needed for your app
+  title: string;
+  description?: string;
+  priority?: string | number;
+  // ...other fields
+};
