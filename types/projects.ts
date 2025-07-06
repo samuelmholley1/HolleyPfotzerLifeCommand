e3797872bde24d1e3ef4b38f8a6205bb9a273f5c
Samuel Holley <samuelholleyai@gmail.com>
2025-07-05 17:11:44 -0700

    fix: create placeholder types for projects

---
+++ types/projects.ts
@@ -0,0 +1,7 @@
+// types/projects.ts
+
+// Placeholder types to unblock the build.
+// TODO: Define these with strict, accurate types.
+export type Project = any;
+export type CreateProjectData = any;
+export type UpdateProjectData = any;
