29668d10a0194f447508b2b6776f96b77173bfe1
Samuel Holley <samuelholleyai@gmail.com>
2025-07-06 18:22:02 -0700

    fix(config): use dotenv to ensure E2E env vars are loaded

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -1,3 +1,6 @@
+// Load environment variables from .env.local
+require('dotenv').config({ path: './.env.local' });
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
